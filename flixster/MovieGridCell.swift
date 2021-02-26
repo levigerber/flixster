1ac5d846d698f0fc1cdb3e86f70b08d9a80af070
Levi Gerber <lxg5364@psu.edu>
2021-02-26 02:24:47 -0500

    Created Superhero view

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Levi Gerber on 2/26/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
